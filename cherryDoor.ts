49f172d5c0ec5ed770b0eb758c8f3bb5c98c008d
sae220 <mod-sharp.truetle3@outlook.jp>
2024-06-18 16:01:16 +0900

    first commit

---
+++ cherryDoor.ts
@@ -0,0 +1,9 @@
+namespace agent {
+    //% group="Inventory" weight=164
+    //% blockId=minecraftAgentSetCherryDoor block="agent set cherry door|count $count|in slot $slot"
+    //% slot.min=1 slot.max=27
+    //% count.min=1 count.max=64
+    export function setCherryDoor(count: number, slot: number): void {
+        player.execute(`replaceitem entity @c slot.inventory ${slot - 1} cherry_door ${count}`);
+    }
+}
