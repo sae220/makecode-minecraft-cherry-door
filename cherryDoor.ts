eb8b1d8263394d11a1569d760a9da5a6db39236d
Sae <73706334+sae220@users.noreply.github.com>
2025-03-08 09:51:22 +0900

    安山岩の階段

--- cherryDoor.ts
+++ cherryDoor.ts
@@ -1,9 +1,9 @@
 namespace agent {
     //% group="Inventory" weight=164
-    //% blockId=minecraftAgentSetCherryDoor block="agent set cherry door|count $count|in slot $slot"
+    //% blockId=minecraftAgentSetAndesiteStairs block="エージェントに安山岩の階段を$count個、自分のスロット$slot番に設定させる"
     //% slot.defl=1 slot.min=1 slot.max=27
     //% count.defl=1 count.min=1 count.max=64
-    export function setCherryDoor(count: number, slot: number): void {
-        player.execute(`replaceitem entity @c slot.inventory ${slot - 1} cherry_door ${count}`);
+    export function setAndesiteStairs(count: number, slot: number): void {
+        player.execute(`replaceitem entity @c slot.inventory ${slot - 1} andesite_stairs ${count}`);
     }
 }
